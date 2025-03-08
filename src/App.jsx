e138c70ef23a70884d0f943bd693966033b10d57
Hzb <hazeebville@gmail.com>
2025-03-08 16:59:11 +0000

    tailwind configured

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Home from "./components/Home";
+import ResumeBuilder from "./components/ResumeBuilder";
+import CoverLetter from "./components/CoverLetter";
+
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <Home />
+      <ResumeBuilder />
+      <CoverLetter />
+    </>
+  );
+};
+
+export default App;
